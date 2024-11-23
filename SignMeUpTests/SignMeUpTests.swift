20f461d7f38f6d8c70849bd8f0c8bb0c3f5dfdc3
Tanishq Goyal <tanishqgoyal07april@gmail.com>
2024-11-23 16:22:59 +0530

    first commit, landing page and login screen

---
+++ SignMeUpTests/SignMeUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignMeUpTests.swift
+//  SignMeUpTests
+//
+//  Created by TANISHQ on 23/11/24.
+//
+
+import Testing
+@testable import SignMeUp
+
+struct SignMeUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
