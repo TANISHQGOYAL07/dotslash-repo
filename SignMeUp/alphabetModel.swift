797adbd774f0a73106093bede554b72aabd25368
Tanishq Goyal <tanishqgoyal07april@gmail.com>
2024-11-23 18:17:35 +0530

    Added alphabet model to display the alphabets in the dictionary section

---
+++ SignMeUp/alphabetModel.swift
@@ -0,0 +1,15 @@
+//
+//  alphabetModel.swift
+//  SignMeUp
+//
+//  Created by TANISHQ on 23/11/24.
+//
+
+import Foundation
+
+// model for display alphabets and a word of daily use starting with that alphabet
+struct Alphabet {
+    var name: String
+    var dailyUse: String
+}
+
