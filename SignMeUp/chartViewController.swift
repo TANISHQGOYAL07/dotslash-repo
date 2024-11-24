702e5d95a9c28cabbf1c474415230e88e9d25a7f
Tanishq Goyal <tanishqgoyal07april@gmail.com>
2024-11-24 09:27:40 +0530

    Fully project done along with ML models and user friendly interface and innovation or advancement in dictionary

---
+++ SignMeUp/chartViewController.swift
@@ -0,0 +1,33 @@
+//
+//  chartViewController.swift
+//  SignMeUp
+//
+//  Created by TANISHQ on 24/11/24.
+//
+
+import UIKit
+
+class chartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func bckBtnTapped(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
--- SignMeUp/gestureTableViewController.swift
+++ SignMeUp/gestureTableViewController.swift
@@ -87,6 +87,7 @@ class gestureTableViewController: UITableViewController {
         // Return a random color from the array
         return colors.randomElement() ?? .black
     }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
